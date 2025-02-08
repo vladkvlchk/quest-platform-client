5d2f56d985a7c553e07ef98005a78ae238595a55
Vladyslav <prostoandrei9@gmail.com>
2025-02-09 00:48:10 +0200

    fix: build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,12 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
-
 import { authOptions } from "./authOptions";
 
-type CombineRequest = Request & NextApiRequest;
-type CombineResponse = Response & NextApiResponse;
-
-const handler = async (req: CombineRequest, res: CombineResponse) => {
-  return await NextAuth(req, res, {
-    ...authOptions,
-    session: {
-      ...authOptions.session,
-      maxAge: 24 * 60 * 60,
-    },
-  });
-};
+const handler = NextAuth({
+  ...authOptions,
+  session: {
+    ...authOptions.session,
+    maxAge: 24 * 60 * 60,
+  },
+});
 
 export { handler as GET, handler as POST };
