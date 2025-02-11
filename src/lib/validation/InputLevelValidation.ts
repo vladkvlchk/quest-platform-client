e681c74234d7cd1d0953110901fe0b2c226d7712
Vladyslav <prostoandrei9@gmail.com>
2025-02-11 22:07:01 +0200

    fix: input-level form validation

--- src/lib/validation/InputLevelValidation.ts
+++ src/lib/validation/InputLevelValidation.ts
@@ -5,13 +5,12 @@ export const InputLevelFormShema = z.object({
     .string()
     .trim()
     .min(1, "Question is required")
-    .min(10, "Question must be at least 10 characters")
+    .min(3, "Question must be at least 3 characters")
     .max(1000, "Question must be at most 1000 characters"),
   correct_answer: z
     .string()
     .trim()
     .min(1, "Correct answer is required")
-    .min(5, "Correct answer must be at least 5 characters")
     .max(100, "Correct answer must be at most 100 characters"),
   try_limit: z.number().min(1, "Try amount must be at least 1"),
 });
